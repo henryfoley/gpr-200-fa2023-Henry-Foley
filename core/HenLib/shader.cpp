4ab9858019d44771892e2dd3ea0a805912458285
Henry F <henry.foley@mymail.champlain.edu>
2023-09-12 11:15:53 -0400

    Created HenLib and shader files

---
+++ core/HenLib/shader.cpp
@@ -0,0 +1,9 @@
+#include "shader.h"
+
+namespace HenryLib {
+	std::string HenryLib::loadShaderSourceFromFile(const std::string& filePath)
+	{
+		return std::string();
+	}
+}
+
---
+++ core/HenLib/shader.h
@@ -0,0 +1,8 @@
+#pragma once
+
+#include <sstream>
+#include <fstream>
+
+namespace HenryLib {
+	std::string loadShaderSourceFromFile(const std::string& filePath);
+}
